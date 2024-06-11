5fe1676081fb09075a6b767d0de1dc1ed1151c17
CasManny <chukwusabastine2018@gmail.com>
2024-06-11 22:51:06 +0100

    Implemented clerk authentication

---
+++ components/shared/Header.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+const Header = () => {
+  return (
+    <header className="w-full border-b px-5">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
