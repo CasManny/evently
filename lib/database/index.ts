e522075b9e5cb016cda43ae0122eb5775f95dc76
CasManny <chukwusabastine2018@gmail.com>
2024-06-13 13:10:58 +0100

    implemented a connection to mongodb and created the user and event model

---
+++ lib/database/index.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+
+let cached = (global as any).mongoose || { conn: null, promise: null }
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.con;
+
+    if (!MONGODB_URL) throw new Error('MONGODB_URL IS MISSING')
+    
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName: 'eventmeet',
+        bufferCommands: false,
+    })
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
